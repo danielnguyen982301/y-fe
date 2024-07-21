0c8d212013e651d425f53e3f24265e9fbe67f8ce
danielnguyen982301 <danielnguyen982301@gmail.com>
2024-07-22 00:30:34 +0700

    Adjust some files

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,8 +1,8 @@
 'use client';
 
 import { io } from 'socket.io-client';
+import { BACKEND_URL } from './config';
 
-const URL = 'http://localhost:5000';
-const socket = io(URL, { autoConnect: false });
+const socket = io(BACKEND_URL as string, { autoConnect: false });
 
 export default socket;
